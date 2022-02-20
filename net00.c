4313934a771f92590a7a87b128c5cd5c5d41c4f6
IsaacHair <58676928+IsaacHair@users.noreply.github.com>
2022-02-19 16:28:07 -0800

    Update net00.c
    still glitching of course

--- net00.c
+++ net00.c
@@ -4,7 +4,7 @@
 struct node {
   struct node *parent[3];
   struct node *child[3];
-  enum {NAND, XOR, TRANS, ROT, SCALE, IF/ELSE, REG0, REG1, REG2, REG3, ADD, SUB} type;
+  enum {NAND, XOR, TRANS, ROT, SCALE, IFELSE, REG0, REG1, REG2, REG3, ADD, SUB} type;
   int* weights; //include fuzzy memory as well
   int grid[256][256];
   int density0[1024][64];
@@ -17,7 +17,7 @@ void main() {
 }
 
 void link() {
-  time_t time;
+  time_t t;
   char used[100];
   int counter;
   int choice0;
@@ -29,8 +29,8 @@ void link() {
   srand((unsigned) time(&t));
   
   for (counter = 0; counter < 100; counter++)
-    used[scanner] = 0;
-  for (set = 0; set < 3; set++)
+    used[counter] = 0;
+  for (set = 0; set < 3; set++, printf("out set=%dnow\n", set)) {
     for (remaining = 100; remaining > 0;) {
       choice0 = rand() % remaining;
       for (counter = choice0+1, pos0 = 0; 1; pos0++) {
@@ -41,7 +41,7 @@ void link() {
       }
       used[pos0] = 1;
       remaining--;
-      choice0 = rand() % remaining;
+      choice1 = rand() % remaining;
       for (counter = choice1+1, pos1 = 0; 1; pos1++) {
         if (!used[pos1])
           counter--;
@@ -53,14 +53,17 @@ void link() {
       
       network[pos0].parent[set] = &network[pos1];
       network[pos1].child[set] = &network[pos0];
+      
+                    printf("yep %d\n", remaining);
     }
+  }
 }
 
 void test() {
   int i, set;
-  
   for (set = 0; set < 3; set++)
     for (i = 0; i < 100; i++)
       if (&(network[i]) != (network[i].parent[set])->child[set])
         printf("link failure for parent of set %d index %d\n", set, i);
+  printf("test complete\n");
 }
