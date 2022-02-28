4c78920b7084a4e166d35e524fe3c5ec7e87e1eb
IsaacHair <58676928+IsaacHair@users.noreply.github.com>
2022-02-28 10:18:39 -0800

    start working procedure

--- net01.c
+++ net01.c
@@ -1,1 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
 
+struct node {
+  struct node *parent[3];
+  struct node *child[3];
+  enum {NAND, XOR, TRANS, ROT, SCALE, IFELSE, REG0, REG1, REG2, REG3, ADD, SUB} type;
+  int* weights; //include fuzzy memory as well
+  int grid[256][256];
+  int density0[1024][64];
+  int density1[1024][64];
+} network[100];
+
+void link() {
+  time_t t;
+  char used[300]; //divide index by 3 with truncation to get corresponding node
+  int idx;
+  int node;
+  int set;
+  srand((unsigned) time(&t));
+  
+  for (node = 0; node < 100; node++) //ZZZ
+}
+
+void test() {
+  
+}
+
+void main() {
+  link();
+  test();
+  printf("done\n");
+}
