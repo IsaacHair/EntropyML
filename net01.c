f725e0551014a595747602a773a6086d9be587c8
IsaacHair <58676928+IsaacHair@users.noreply.github.com>
2022-02-28 10:46:34 -0800

    might work, untested

--- net01.c
+++ net01.c
@@ -14,12 +14,33 @@ struct node {
 void link() {
   time_t t;
   char used[300]; //divide index by 3 with truncation to get corresponding node
+  int amountLeft;
   int idx;
   int node;
   int set;
+  int tempChildAmount;
+  int tempChildLocation;
   srand((unsigned) time(&t));
   
-  for (node = 0; node < 100; node++) //ZZZ
+  for (idx = 0; idx < 300; idx++)
+    used[idx] = 0;
+  amountLeft = 300;
+  for (node = 0; node < 100; node++)
+    for (set = 0; set < 3; set++) {
+      tempChildAmount = rand()%amountLeft;
+      tempChildLocation = tempChildAmount; //abusing this to count
+      for (idx = 0; idx < 300; idx++) {
+        if (!used[tempChildLocation])
+          tempChildLocation--;
+        if (tempChildLocation < 0)
+          break;
+      }
+      tempChildLocation = idx;
+      network[node].child[set] = &network[tempChildLocation/3];
+      network[tempChildLocation/3].parent[tempChildLocation%3] = &network[node];
+      amountLeft--;
+      used[tempChildLocation] = 1;
+    }
 }
 
 void test() {
